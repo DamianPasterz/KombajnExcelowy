13d7958ffde80e2423fdf36240ee4633f8cb9d9f
Kamil Swider <kamil.b.swider@gmail.com>
2021-12-16 19:50:28 +0100

    Kutas numer tfa

--- main.js
+++ main.js
@@ -21,21 +21,20 @@ function processExcel(data) {
   var workbook = XLSX.read(data, {
     type: 'binary'
   });
-  console.log(workbook);
 
   var data = to_json(workbook);
   return data
 };
 
 function to_json(workbook) {
-  var result = {};
+  var result = [];
 
   workbook.SheetNames.forEach(function(sheetName) {
     var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
       header: 1
     });
 
-    if (roa.length) result[sheetName] = roa;
+    if (roa.length) result.push(roa);
   });
 
   // return JSON.stringify(result, 2, 2);
@@ -43,6 +42,37 @@ function to_json(workbook) {
 };
 
 function displayContents(contents) {
+  const transformedData = parseData(contents);
+
   let element = document.getElementById('file-content');
   element.textContent = contents;
+}
+
+function parseData(contents) {
+  const sheet1 = contents[0];
+  const filtered = sheet1.filter(row => row[2] > 4 && row[2] < 9);
+  console.log(filtered);
+  const filteredMap = filtered.map(row => row[2] * 2);
+  console.log(filteredMap);
+  const filteredReduce = filteredMap.reduce((acc, value) => {
+    // console.log(value);
+    const transformedValue = value / 2;
+    return [...acc, transformedValue]
+  }, []);
+  const filteredFind = sheet1.find(row => {
+    console.log(row[2] === 2);
+    return row[2] === 2;
+  });
+  console.log(filteredFind);
+  // const value = contents[0][7][2];
+  // console.log(value);
+
+  // filter
+  // map
+  // reduce
+  // find
+  // some
+  // every
+
+  return;
 }
