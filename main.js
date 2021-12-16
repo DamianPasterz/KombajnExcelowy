8f8efda378e74f143b1b59f0d3704292c7b9258d
Damian Pasterz <damian.pasterz@gmail.com>
2021-12-16 18:21:23 +0100

    smutek Kamilu smutek

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
+
 function readSingleFile(e) {
+
     var file = e.target.files[0];
     if (!file) {
         return;
     }
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.onload = function (e) {
-        var contents = e.target.result;
+        let contents = e.target.result;
         displayContents(contents);
+        // console.log(e.target.files);
+
     };
     reader.readAsText(file);
+    console.log(file);
+
 }
 
 function displayContents(contents) {
-    var element = document.getElementById('file-content');
+    let element = document.getElementById('file-content');
     element.textContent = contents;
 }
 
 document.getElementById('file-input')
     .addEventListener('change', readSingleFile, false);
+
+
+const XLSX = require("xlsx");
+// import xlsx from 'xlsx';
+
+const wordkbook = XLSX.readFile(file)
+const worksheet = wordkbook.Sheets["Arkusz1"];
+
+const arrStudentd = XLSX.utils.sheet_add_json(worksheet);
+console.log(arrStudentd);
+
+
+
+
+
