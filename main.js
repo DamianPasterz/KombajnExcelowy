015934ac9719a27950ca41d1e43e96e8e26d844e
Damian Pasterz <damian.pasterz@gmail.com>
2022-06-23 22:59:53 +0200

    repair  display m2

--- main.js
+++ main.js
@@ -8,16 +8,12 @@ function readSingleFile(e) {
     var r = new FileReader();
     r.onload = e => {
       var contents = processExcel(e.target.result);
-      console.log(contents);
       displayContents(contents)
-
     }
     r.readAsBinaryString(file);
   } else {
-    console.log("Failed to load file");
+    alert("Failed to load file");
   }
-
-
 }
 
 function processExcel(data) {
@@ -31,7 +27,6 @@ function processExcel(data) {
 
 function to_json(workbook) {
   var result = [];
-
   workbook.SheetNames.forEach(function (sheetName) {
     var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {
       header: 1
@@ -39,37 +34,20 @@ function to_json(workbook) {
 
     if (roa.length) result.push(roa);
   });
-
-
   return result;
 };
 
 function displayContents(contents) {
   const transformedData = parseData(contents);
-
 }
-
-
-
-
-
-
-
-
-
-
-
 
 function parseData(contents) {
   const sheet1 = contents[0];
   const uniqueColumneValues = [...new Set(sheet1.map(row => row[6]))];
   const uniqueMaterialCodes = uniqueColumneValues.slice(1)
-  console.log(uniqueMaterialCodes);
-
   const columneNineValues = sheet1.map(row => row[9]);
-  // console.log(columneNineValues);
   const allMeters = columneNineValues.slice(1);
-  console.log(allMeters);
+
 
 
 
@@ -79,13 +57,12 @@ function parseData(contents) {
   for (let index = 0; index < uniqueMaterialCodes.length; index++) {
     const filterted = sheet1.filter(value => value[6] === uniqueMaterialCodes[index])
     const sum = filterted.reduce((acc, value) => {
-      return acc + Math.round(value[9]);
+      return acc + value[9];
     }, 0);
     result[uniqueMaterialCodes[index]] = sum;
   };
-  resultaArray = Object.entries(result);
 
-  console.log(result);
+  resultaArray = Object.entries(result);
 
 
 
@@ -93,40 +70,34 @@ function parseData(contents) {
     metersSquare += allMeters[index1];
   };
 
-
-
-
-  console.log(Math.floor(metersSquare));
-
-
-
   const h2 = document.createElement('h2');
   h2.textContent = `suma metrów: ${Math.round(metersSquare)}`;
   document.body.appendChild(h2)
   const field = document.querySelector('.reesault__field');
-
   pozycja = Object.entries(result);
-  console.log(pozycja);
-
-
-
 
   for (let wer = 0; wer < pozycja.length; wer++) {
-
-    console.log(pozycja[wer]);
     let newArr = pozycja[wer]
-    console.log(newArr[0]);
 
     field.innerHTML += `
                     <div class="reesault__row">
                     <div class="material">${newArr[0]}</div>
-                    <div class="meter">${newArr[1]}</div>
+                    <div class="meter">${newArr[1].toFixed(1)}</div>
                     </div>
             
             `
     field.classList.add('reesault__field__end')
+  }
+  field.innerHTML += `<button class="download">POBIERZ</button>`;
 
+  const button = document.querySelector('button');
+
+  button.onclick = function saveXls() {
+    var worksheet2 = XLSX.utils.aoa_to_sheet(pozycja);
+    console.log(worksheet2);
+    const newWoerkBook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(newWoerkBook, worksheet2, "haha")
+    XLSX.writeFile(newWoerkBook, "hart2.xlsx")
 
   }
 }
-
